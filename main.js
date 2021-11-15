abfea802c966156da105a6543064f08128f3ad14
FasDot1 <fas.1@hotmail.co.uk>
2021-11-15 21:11:07 +0000

    javaScript

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-    
+    window.onload = () => {
+        const form1 = document.querySelector("#addForm");
+
+        let items = document.getElementById("items");
+        let submit = document.getElementById("submit");
+
+        let editItem = null;
+
+        form1.addEventListener("submit", addItem);
+        items.addEventListener("click", removeItem);
+    } ;
+
+    function addItem(e) {
+        e.preventDefault();
+
+        if (submit.value != "Submit") {
+            console.log("Hello");
+
+            editItem.target.parantNode.childNodes[0].data = document.getElementById("item").value;
+
+
+        }
+
+    }
